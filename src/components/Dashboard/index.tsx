29f6a197427761ed759cbfb521de73e94cb3908e
Leonardo <lporchat06@gmail.com>
2022-05-16 12:19:07 -0300

    stylizando a parte das tabelas e heads

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,12 @@
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+import { Container } from "./style";
+
 export function Dashboard() {
-  return(
-    
-  )
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,57 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/07/1999</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
